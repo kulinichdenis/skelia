b4680f2d1b8c24d4fa370339e9069760b32ae58c
kulinich denis <kulinichdenis@kulinichs-MacBook-Pro.local>
2017-10-08 23:01:05 +0300

    move fake data to file

--- src/middleware/sagas.js
+++ src/middleware/sagas.js
@@ -1,134 +1,22 @@
 import { call, put, takeEvery, select } from 'redux-saga/effects'
-import { FETCH_USER,
-  LOG_OUT,
-  DEFAUTL_USER_STATE,
-  SEND_EMAIL
-} from 'constants/index'
+import { FETCH_USER, LOG_OUT, DEFAUTL_USER_STATE, SEND_EMAIL } from 'constants/index'
 import { fetchUserSuccess,
-  fetchUserError,
-  defaultUserState,
-  setDraftId,
-  setPrices,
+  fetchUserError, defaultUserState,
+  setDraftId, setPrices,
   emailStatus
 } from 'reducers/user'
 import { start_progress, stop_progress } from 'reducers/progress'
 import { authUser, getDrafts, logoutUser, url, requestCreator } from 'utils/apiClient'
-import { tokenState, draftState, emailSelector } from 'utils/helpers'
-
-const data = { "language" : "nl",
-  "partnerReference": [
-        {
-           "key": "salesId",
-            "value":"12345678"
-        },       
-        {
-          "key":"productId",
-            "value":"235689"
-        }     
-    ],
-    "policyholder": {
-      "address": {
-          "city": "Bruxelles",
-          "country": "BE",
-          "number": "44",
-          "street": "Rue des palais"
-      },
-      "billingAddress": {
-          "city" : "Bruxelles",
-            "country" : "BE",
-            "number" : "45",
-            "street" : "Rue   des   palais"                                         
-      },
-      "person": {
-        "birthDate": "1984-12-16",
-        "firstName": "John",
-        "lastName": "Doe",
-        "title": "Mr"
-      },
-      "contact": {
-            "email": "admin@example.com",
-            "phone": "+3226592323"
-      }        
-    },
-    "risk": {
-      "drivers": [
-            {
-                "contact": {  
-                  "email": "john.doe@example.com",
-                  "phone": "+32478595959"    
-                }, 
-                "numberOfClaimsLast5Years": "0",
-                "person" :  {
-            "birthDate": "1984-12-16",
-            "firstName": "John",
-            "lastName": "Doe",
-            "title": "Mr"
-                }
-           }
-        ],
-        "vehicle" :  {
-            "cascoInsurerCompany": "AXA Belgium", 
-            "details": {
-                "code": "123470",
-                "codeType": "NAT",
-                "country": "BE",
-                "vehicleType": "10"
-            },        
-            "firstRegistrationYear" :"2017",
-            "identification":  {
-                "registration": {
-                    "country": "BE",
-                    "plate": "1-ABC-123"
-                }
-            },               
-            "invoice" :   {
-                "isPurchasedInLast6Months": true,
-                "purchasedPriceIncludingVat": {
-                    "currency": "EUR",
-                    "value" : 1500000
-               }
-            }
-        }    
-    },
-    "terms":   {
-      "claimLimit":  {
-        "currency":  "EUR",
-        "value":  1600000   
-      },
-        "requestPaperCopy": false,
-        "startDate": "2017-12-17",
-        "variant": "GAP60"
-    }        
-}
-
-const dataPrice = {
-  "partnerReference": [
-    { "key": "salexId",
-      "value": "12345678"
-    }
-  ]
-}
-
-const emailData = {
-  "partnerReference": [
-    {
-      "key": "salesId",
-      "value": "12345678"
-    }, {
-      "key": "productId",
-      "value": "235689"
-    }  
-  ],            
-  "transport": "EMAIL"
-}
+import { tokenSelector, draftSelector, emailSelector } from 'utils/helpers'
+import { mainData, dataPrice, emailData } from 'utils/fakeData'
 
 function* fetchUser(action) {
   yield put(start_progress())
   try {
     const auth = yield call(requestCreator, action.payload, url.auth)
     yield put(fetchUserSuccess(auth.data))
-    const result = yield call(requestCreator, data, url.drafts)
-    const { draftId } = result.data // draftId to store
+    const result = yield call(requestCreator, mainData, url.drafts)
+    const { draftId } = result.data
     yield put(setDraftId(draftId))
     const price = yield call(requestCreator, dataPrice, url.prices(draftId))
     yield put(setPrices(price.data.prices))
@@ -141,7 +29,7 @@ function* fetchUser(action) {
 }
 
 function* logout(action) {
-  const token = yield select(tokenFromUserData)
+  const token = yield select(tokenSelector)
   try {
     const result = yield call(logoutUser, token)
     yield put(defaultUserState())
@@ -153,9 +41,9 @@ function* logout(action) {
 }
 
 function * sendEmail() {
-  const draft = yield select(draftState)
+  const draft = yield select(draftSelector)
   const { email } = yield select(emailSelector)
-  debugger
+
   yield put(start_progress())
   try {
     const requestEmail = yield call(requestCreator, emailData, url.email(draft))
